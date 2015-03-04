5189a3dbcff6718f9b766a9da904ab289a49c028
ahmedzein <elze0010@algonquinlive.com>
2015-03-04 17:43:57 -0500

    css +js
    bounce btn
    added js code
    added animation
    
    append&expand
    added the js code

--- js/main.js
+++ js/main.js
@@ -7,14 +7,34 @@ var $diamond =$('.diamond');
 var $btnCollapseExpand =$('.btn-collapse-expand');
 var $panel =$('.panel');
 
+var $btnBounce =$('.btn-bounce');
+var $circle =$('.circle');
+
+var $btnAppend =$('.btn-append');
+var $list =$('.list');
+
+
 $btnShowHide.on('click', function()  {
     $box.toggleClass ('box');
 });
 
 $btnMove =$('click', function() {
-    $diamond.remove ('diamond');
+    $diamond.toggleClass ('diamond');
 });
 
 $btnCollapseExpand.on('click', function() {
     $panel.toggleClass('panel');
+});
+
+$btnBounce.on('click', function() {
+    $circle.addClass('js-circle-bounce')
+});
+
+$circle.on ('animationend', function(){
+    $circle.removeClass('js-circle-bounce');
+});
+
+$btnAppend.on('click', function() {
+    var $li =$('<li>New List Item</li>');
+    $list.prepend($li);
 });
